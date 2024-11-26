3b56d04374f5e0bacf86799a86a5dfb6978fc75e
AmritkThapa <amritthapa.6024@gmail.com>
2024-11-26 10:35:48 +0545

    Project Initialized

---
+++ src/main/java/com/amrit/firebaseFCM/FirebaseFcmApplication.java
@@ -0,0 +1,13 @@
+package com.amrit.firebaseFCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseFcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseFcmApplication.class, args);
+	}
+
+}
